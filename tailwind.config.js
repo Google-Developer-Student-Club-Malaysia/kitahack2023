9e8d5a8125151428ef6983935b4d5eff880aced9
Tae Woo Kim <taewookim02@gmail.com>
2022-12-31 00:25:08 +0800

    Add basic styling to countdown section

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ module.exports = {
   content: ["./**/*.{html,js}", "*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      "google-red": "#E33835",
+      "google-blue": "#4580E8",
+      "google-green": "#329C55",
+      "google-orange": "#f9ab00",
+    },
   },
   plugins: [
     require("tailwindcss"),
